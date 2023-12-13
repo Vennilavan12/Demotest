56eff877870969bd1e8890a66c247401acb88514
Vennilavan <77039703+Vennilavan12@users.noreply.github.com>
2023-12-13 16:36:02 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,4 +6,6 @@ docker info
 docker ps
 docker pull nginx:alpine
 docker images
-
+docker tag nginx:alpine vennilavan/prod
+docker login -u vennilavan -p dckr_pat_S24xwX_73dmdhHpTqKr-3UVNqds
+docker push vennilavan/prod
