71c702478f1d02da5a14a532849b250672fc3ef4
Vennilavan <77039703+Vennilavan12@users.noreply.github.com>
2023-12-14 12:50:04 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,8 +4,6 @@ echo "This is Jenkins"
 docker images
 docker info
 docker ps
-docker pull nginx:alpine
+docker pull vennilavan/dev:latest
 docker images
-docker tag nginx:alpine vennilavan/prod
-docker login -u vennilavan -p dckr_pat_S24xwX_73dmdhHpTqKr-3UVNqds
-docker push vennilavan/prod
+docker run -d -p 80:80 vennilavan/dev:latest
