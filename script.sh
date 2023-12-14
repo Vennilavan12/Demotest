bfbc54f2e023396a95619adc73ca87c949570f5d
Vennilavan <77039703+Vennilavan12@users.noreply.github.com>
2023-12-14 12:54:16 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ echo "This is Jenkins"
 docker images
 docker info
 docker ps
-docker system prune
+docker stop $(docker ps -a -q)
 docker pull vennilavan/dev:latest
 docker images
 docker run -d -p 80:3000 vennilavan/dev:latest
