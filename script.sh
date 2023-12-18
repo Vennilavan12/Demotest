94f55e58fbbb372d9335ba5026f43133c302a058
Vennilavan <77039703+Vennilavan12@users.noreply.github.com>
2023-12-18 13:40:08 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,3 @@ echo "This is Jenkins build triggers setup"
 docker images
 docker info
 docker ps
-docker stop $(docker ps -a -q)
-docker pull vennilavan/dev:latest
-docker images
-docker run -d -p 80:3000 vennilavan/dev:latest
